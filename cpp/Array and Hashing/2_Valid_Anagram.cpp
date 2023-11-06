183acf5cedaca5a843d88a4653cb6b729247f7ba
Harshit Srivastava <91839831+raunakk07@users.noreply.github.com>
2023-11-06 21:36:03 +0530

    Create 2_Valid_Anagram.cpp

---
+++ cpp/Array and Hashing/2_Valid_Anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int arr[27] = {0};
+        int n = s.length();
+        if(n != t.length()) return false;
+        for(int i = 0;i<n;i++) arr[s[i]-97]++;
+        for(int i = 0;i<n;i++) arr[t[i]-97]--;
+
+        for(int i = 0;i<=26;i++){
+            if(arr[i] !=0 ) return false;
+
+        }return true;
+
+    }
+};
